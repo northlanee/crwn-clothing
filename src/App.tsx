5e3c0921ceee2259defa6f7130a6bc355a6d76b3
northlanee <iwasmusic16@gmail.com>
2020-09-04 13:33:40 +0300

    Development initial fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div className="App">111</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
