44321cc9ded67bb7e77b5fbf62db355624fc8ed0
northlanee <iwasmusic16@gmail.com>
2020-09-09 13:25:18 +0300

    firebase deploying added

--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -4,16 +4,7 @@ import "firebase/auth";
 
 import { User } from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAFKr_9m6W_GEi1l7qVEDr-zLU-w7RGUtg",
-  authDomain: "crwn-db-ec261.firebaseapp.com",
-  databaseURL: "https://crwn-db-ec261.firebaseio.com",
-  projectId: "crwn-db-ec261",
-  storageBucket: "crwn-db-ec261.appspot.com",
-  messagingSenderId: "651629948345",
-  appId: "1:651629948345:web:377fe09dab2eab1dd51a68",
-  measurementId: "G-YK8ZK69XKL",
-};
+import { firebaseConfig } from "./config";
 
 export const getUserProfileDocument = async (
   user: User,
